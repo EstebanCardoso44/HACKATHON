4e0853bf1be066f84cd308f24b87e796282c04d6
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-03 10:13:58 +0200

    Background + paragraph

--- home.php
+++ home.php
@@ -7,13 +7,9 @@
  <div id="container">
  <form action="verification.php" method="POST">
  <h1>Bienvenue sur le VPN d'Ynov !</h1>
+ <p>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.</p>
  <input type="submit" id='submit' value='Se Connecter au VPN' >
  <?php
- if(isset($_GET['erreur'])){
- $err = $_GET['erreur'];
- if($err==1 || $err==2)
- echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
- }
  ?>
  </form>
  </div>
