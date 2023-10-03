d4cbcf498d474ad4e3a406e9cf477ed9f92d5e07
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-03 11:50:22 +0200

    config api

--- home.php
+++ home.php
@@ -5,10 +5,9 @@
  </head>
  <body>
  <div id="container">
- <form action="http://localhost/hackathon/HACKATHON/index.php">
+ <form>
  <h1>Bienvenue sur le VPN d'Ynov !</h1>
  <p>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.</p>
- <input type="submit" id='submit' value='Se Connecter au VPN' >
  <?php
  ?>
  </form>
