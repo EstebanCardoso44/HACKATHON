de25da0c99ae31231bef4e8d791ffd85dc660cd2
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-03 09:48:18 +0200

    Changement contenus du cadre d'accueil

--- home.php
+++ home.php
@@ -6,12 +6,8 @@
  <body>
  <div id="container">
  <form action="verification.php" method="POST">
- <h1>Connexion</h1>
- 
- <input type="text" placeholder="Entrer votre email" name="email" required>
-
-
- <input type="submit" id='submit' value='LOGIN' >
+ <h1>Bienvenue sur le VPN d'Ynov !</h1>
+ <input type="submit" id='submit' value='Se Connecter au VPN' >
  <?php
  if(isset($_GET['erreur'])){
  $err = $_GET['erreur'];
