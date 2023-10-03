0ac95c3c5f2351bd09be80e279df847cf75108fc
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-03 16:25:38 +0200

    new home with link register login

--- php_template/home.php
+++ php_template/home.php
@@ -1,17 +1,25 @@
+<?php
+session_start();
+include "header.php";
+?>
+
+<!DOCTYPE html>
 <html>
- <head>
- <meta charset="utf-8">
- <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
- </head>
+
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+</head>
+
  <body>
+<div class="login-register-container">
  <div id="container">
- <form action="http://localhost/hackathon/HACKATHON/index.php">
+ <form action="http://localhost/HACKATHON/php_template/RegisterHtml.php">
  <h1>Bienvenue sur le VPN d'Ynov !</h1>
  <p>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.</p>
- <input type="submit" id='submit' value='Se Connecter au VPN' >
- <?php
- ?>
  </form>
+ </div>
  </div>
  </body>
 </html>
