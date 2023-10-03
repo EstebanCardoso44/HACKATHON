a838e9cac9861e94c8d96a03b4b671a1570cf1fe
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-03 10:33:48 +0200

    Redirection on button

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
  </head>
  <body>
  <div id="container">
- <form action="verification.php" method="POST">
+ <form action="http://localhost/hackathon/HACKATHON/index.php">
  <h1>Bienvenue sur le VPN d'Ynov !</h1>
  <p>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.</p>
  <input type="submit" id='submit' value='Se Connecter au VPN' >
