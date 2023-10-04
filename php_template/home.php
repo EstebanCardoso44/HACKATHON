a2da9b5b57126cc9066e77776e6109d4c0c0a5ba
IceK37 <45316097+IceK37@users.noreply.github.com>
2023-10-04 14:14:48 +0200

    session verification on home fix

--- php_template/home.php
+++ php_template/home.php
@@ -14,15 +14,35 @@ include "header.php";
 
  <body>
 <div class="login-register-container">
+
+<?php
+if (!isset($_SESSION['userID']) || !isset($_SESSION['username'])) {
+?>
+<div class="alert-container">
+        <div class="alert-message">
+          Vous n'êtes pas connecté.
+        </div>
+    </div>
+	<?php
+}?>
+
  <div id="container">
  <form>
  <h1>Bienvenue sur le VPN d'Ynov !</h1>
- <p>YVP : acronyme de "Ynov Virtual Private Network", est un service de réseau privé virtuel spécialement conçu pour répondre aux besoins de l'école Ynov. Développé pour garantir la sécurité et la confidentialité optimale de tous les membres de la communauté Ynov, comme les étudiants, les enseignants, et le personnel administratif.</p>
- <a href="http://localhost/HACKATHONr/php_template/loginhtml.php"> 
+ <p>Ynov Virtual Private Network, est un service de réseau privé virtuel spécialement conçu pour répondre aux besoins de l'école Ynov. Développé pour garantir la sécurité et la confidentialité optimale de tous les membres de la communauté Ynov, comme les étudiants, les enseignants, et le personnel administratif.</p>
+ 
  <?php
- ?>
+ if (!isset($_SESSION['userID']) || !isset($_SESSION['username'])) {
+?>
+<p>Vous n'êtes pas encore connecté ! Vous n'avez donc pas accès aux différentes pages de ce site, veuilez vous connecter ou vous enregistrer.<p>
+ <a href="../php_template/registerhtml.php" class="button">S'enregistrer</a>
+<a href="../php_template/loginHtml.php" class="button">Se connecter</a>
+<?php
+}?>
+
 </form>
  </div>
  </div>
  </body>
 </html>
+<?php
