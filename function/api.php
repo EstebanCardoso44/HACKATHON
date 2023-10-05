4dd68bae8c369b5bdcb32235ece91d4927e93c3d
ARAYNAUD <adrien.raynaud@ynov.com>
2023-10-05 14:58:47 +0200

    api

--- function/api.php
+++ function/api.php
@@ -8,11 +8,7 @@ if($result) {
     header("Content-Type: JSON");
     $i=0;
     while($row = mysqli_fetch_assoc($result)){
-    $response[$i]['id'] = $row['id'];
-    $response[$i]['Name'] = $row['Name'];
     $response[$i]['Email'] = $row['Email'];
-    $response[$i]['confirmkey'] = $row['confirmkey'];
-    $response[$i]['password'] = $row['password'];
     $i++;}
 }
 echo json_encode($response, JSON_PRETTY_PRINT);
